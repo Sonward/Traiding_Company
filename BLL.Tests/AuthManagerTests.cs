89dcd423e770439759bc6f0f81b9a5fa3a03f673
Sonward <70655176+Sonward@users.noreply.github.com>
2020-11-04 13:35:24 +0200

    Add files via upload

---
+++ BLL.Tests/AuthManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using BuisnesLogicLayer.Concrete;
+using DAL.Intefaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Tests
+{
+    [TestFixture]
+    class AuthManagerTests
+    {
+        private Mock<ICustomerDataDAL> custDatDAl;
+        private AuthManager manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            custDatDAl = new Mock<ICustomerDataDAL>(MockBehavior.Strict);
+
+            manager = new AuthManager(custDatDAl.Object);
+        }
+
+        [Test]
+        public void LoginSellerTest()
+        {
+            string username = "user";
+            string password = "pass";
+
+            custDatDAl.Setup(d => d.Login(username, password)).Returns(true);
+            var res = manager.Login(username, password);
+
+            Assert.IsTrue(res);
+        }
+    }
+}
---
+++ BLL.Tests/ConNodeManagerTest.cs
@@ -0,0 +1,47 @@
+﻿using BuisnesLogicLayer.Concrete;
+using DAL.Intefaces;
+using Moq;
+using System.Data.SqlTypes;
+using DTO;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Tests
+{
+    [TestFixture]
+    class ConNodeManagerTest
+    {
+        private Mock<IConnectionNodeDAL> conNodDAL;
+        private ConNodeManager manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            conNodDAL = new Mock<IConnectionNodeDAL>(MockBehavior.Strict);
+
+            manager = new ConNodeManager(conNodDAL.Object);
+        }
+
+        public void AddNodeTest()
+        {
+            ConnectionNodeDTO inConNode = new ConnectionNodeDTO
+            {
+                CustomerDataId = 1,
+                ItemId = 1,
+                QuantityOfItem = 5,
+                TimeOfReceipt = SqlDateTime.MinValue,
+                SendingTime = SqlDateTime.Null,
+                StatusId = 1
+            };
+
+            var res = manager.AddNode(inConNode);
+
+            Assert.IsNotNull(res);
+        }
+
+    }
+}
